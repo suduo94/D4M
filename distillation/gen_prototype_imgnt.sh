94f7a3e3117db183be28097b4cd8bc2ac41fec34
suduo <duosu94@gmail.com>
2024-07-24 14:30:09 +0800

    modified files

--- distillation/gen_prototype_imgnt.sh
+++ distillation/gen_prototype_imgnt.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=1 python gen_prototype.py \
+CUDA_VISIBLE_DEVICES=0 python gen_prototype.py \
     --batch_size 10 \
-    --data_dir /home-ext/tbw/suduo/data/imagenet \
+    --data_dir ../data/imagenet \
     --dataset imagenet \
-    --diffusion_checkpoints_path /home-ext/tbw/suduo/D3M/stablediffusion/checkpoints/stable-diffusion-v1-5 \
+    --diffusion_checkpoints_path ../stablediffusion/checkpoints/stable-diffusion-v1-5 \
     --ipc 10 \
     --km_expand 1 \
-    --label_file_path /home-ext/tbw/suduo/D3M/label-propmt/imagenet_classes.txt \
-    --save_prototype_path /home-ext/tbw/suduo/D3M/prototypes
+    --label_file_path ./label-propmt/imagenet_classes.txt \
+    --save_prototype_path ./prototypes
---
+++ distillation/gen_syn_image_imgnt.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0 python gen_syn_image_imgnt.py \
+    --dataset imagenet \
+    --diffusion_checkpoints_path ../stablediffusion/checkpoints/stable-diffusion-v1-5 \
+    --guidance_scale 8 \
+    --strength 0.7 \
+    --ipc 10 \
+    --km_expand 1 \
+    --label_file_path ./label-propmt/imagenet_classes.txt \
+    --prototype_path ./prototypes/imagenet-ipc10-kmexpand1.json \
+    --save_init_image_path ../data/distilled_data/
